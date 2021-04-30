b7e1c3e2b23f95015e82dea216267cfc13a471ee
svettom03 <shawnvettom@gmail.com>
2021-04-30 18:23:25 -0400

    added functionality to toggle light based on serial input

--- arduino_pi_communication/arduino_pi_communication.ino
+++ arduino_pi_communication/arduino_pi_communication.ino
@@ -1,11 +1,37 @@
+#define LED_1_PIN 9
+
+bool priorValue = true;
+
 void setup() {
   Serial.begin(57600);
 }
 
+String checkIfNewInstruction(){
+  if(Serial.available() > 0){
+    return "";
+  }
+  String data = Serial.readStringUntil('\n');
+  Serial.print("You sent me: ");
+  Serial.println(data);
+  return data;
+}
+
+void activateLight(bool on){
+  if(on){
+    digitalWrite(LED_1_PIN, HIGH);
+  }else{
+    digitalWrite(LED_1_PIN, LOW);
+  }
+
+}
+
 void loop() {
-  if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    Serial.print("You sent me: ");
-    Serial.println(data);
+  String command = checkIfNewInstruction();
+  if(command != ""){
+    if(command == "on"){
+      activateLight(true);
+    }else{
+      activateLight(false);
+    }
   }
 }

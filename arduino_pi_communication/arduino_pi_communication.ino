c76a9c4d7cf990efffb8cad81011a30b72895ac6
svettom03 <shawnvettom@gmail.com>
2021-05-02 12:41:07 -0400

    funcational display of patterns

--- arduino_pi_communication/arduino_pi_communication.ino
+++ arduino_pi_communication/arduino_pi_communication.ino
@@ -1,8 +1,11 @@
-#define LED_1_PIN 9
 
-bool priorValue = true;
-
+#define LIGHT_DELAY 1
 void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+  setAllLights(false);
   Serial.begin(57600);
 }
 
@@ -14,24 +17,84 @@ String checkIfNewInstruction(){
   Serial.print("You sent me: ");
   Serial.println(data);
   return data;
+  delay(500);
 }
 
-void activateLight(bool on){
+void setAllLights(bool onOrOff){
+  for(int postivePin = 0; postivePin < 32; postivePin++){
+    for(int negativePin = 0; negativePin < 16; negativePin++){
+      setLight(postivePin, negativePin, onOrOff); 
+    }
+  }
+}
+
+int logicalPinToPhysicalPin(int logicalPin){
+  // Mega board doesn't have all pins going in order so this
+  // flips the first 13 to make then go in order
+  if(logicalPin > 13){
+    return logicalPin;
+  }
+  return 13 - logicalPin;
+}
+
+int positivePinToPhysicalPin(int positivePin){
+  // Positive pins are pins 16-47
+  return logicalPinToPhysicalPin(positivePin + 16);
+}
+
+int negativePinToPhysicalPin(int negativePin){
+  // Negative pins are pins 0-15
+  return logicalPinToPhysicalPin(negativePin);
+}
+
+void setLight(int postivePin, int negativePin, bool on){
+  int physicalPosPin = positivePinToPhysicalPin(postivePin);
+  int physicalNegPin = negativePinToPhysicalPin(negativePin);
   if(on){
-    digitalWrite(LED_1_PIN, HIGH);
+    digitalWrite(physicalPosPin, HIGH);
+    digitalWrite(physicalNegPin, LOW);
   }else{
-    digitalWrite(LED_1_PIN, LOW);
+    digitalWrite(physicalPosPin, LOW);
+    digitalWrite(physicalNegPin, HIGH);
   }
 
+}
+
+void displayPattern(String pattern, int iterations){
+
+  Serial.println("displayPattern");
+  for(int iteration = 0; iteration < iterations; iteration++){
+    int positivePin = 0;
+    int negativePin = 0;
+    for(int i = 0; i < pattern.length(); i++){
+      // increment where we are in the 2D array
+      if(pattern[i] == ':'){
+        positivePin++;
+        negativePin = 0;
+        continue;
+        //don't handle lights on separator
+      }
+      if(pattern[i] != '0'){
+        setLight(positivePin, negativePin, true);
+        delay(LIGHT_DELAY);
+        setLight(positivePin, negativePin, false);
+      }
+      negativePin++; 
+    }
+  }
 }
 
 void loop() {
-  String command = checkIfNewInstruction();
-  if(command != ""){
-    if(command == "on"){
-      activateLight(true);
-    }else{
-      activateLight(false);
-    }
-  }
+  
+  String basicPattern = "11:11";
+  displayPattern(basicPattern, 500);
+  delay(1000);
+
+  basicPattern = "10:10";
+  displayPattern(basicPattern, 500);
+  delay(1000);
+  
+  //String command = checkIfNewInstruction();
+  //if(command != ""){
+  //}
 }
